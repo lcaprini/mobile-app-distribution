ccaeb390a3e8c8ca4c1f4651c5c8fc8fb346dd84
Luca Caprini <luca.caprini@eng.it>
2020-05-24 18:03:11 +0200

    New dist directory in wd

--- remote/wd/src/main.js
+++ remote/wd/src/main.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-new */
+/* eslint-disable import/no-webpack-loader-syntax */
 import Vue from 'vue';
 import VueResource from 'vue-resource';
 import App from './App.vue';
@@ -5,6 +7,9 @@ import iView from 'iview';
 import 'iview/dist/styles/iview.css';
 import FastClick from 'fastclick';
 import VueScrollTo from 'vue-scrollto';
+
+require('file-loader?name=[name].[ext]!../index.html');
+require('file-loader?name=[name].[ext]!../builds.json');
 
 FastClick.attach(document.body);
 
